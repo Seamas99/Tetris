c0954a0b595762d47a0034aa799ba13a4e390b4a
Seamas99 <mclaughlin-s65@ulster.ac.uk>
2024-02-16 09:05:16 +0000

    Added Block, IBlock, and Position classes

---
+++ Tetris/Block.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position[][] StartOffset { get; }
+        public abstract int Id { get; }
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState]) 
+            {
+            yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+    }
+}
---
+++ Tetris/Position.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Position
+    {
+        private int row;
+        private int column;
+        public int Row 
+        { 
+            get { return row;  }
+            set { Row = value; }
+        }
+        public int Column
+        {
+            get { return column; }
+            set { Column = value; }
+        }
+        public Position(int row, int column) 
+        {  
+            Row = row;
+            Column = column; 
+        }
+    }
+}
